444b67629e2638fad957d37230bdedde8a015ede
VenomC312 <63124013+venomc312@users.noreply.github.com>
2021-07-07 17:45:58 -0400

    Initial location framework
    +Location functions
    +Location add command (!locadd <location>)

--- venomulx/lua/ulx/modules/sh/cc_trainbuild.lua
+++ venomulx/lua/ulx/modules/sh/cc_trainbuild.lua
@@ -1,3 +1,60 @@
+ulx.GotoExt = {}
+ulx.GotoExt.Locations = {}
+
+// Global Variables
+local tabf = file.Read("ulx/locs.txt","DATA") or ""
+local map = game.GetMap()
+
+// Loc Management functions and hooks
+ulx.GotoExt.Locations = util.JSONToTable(tabf)
+
+if !ulx.GotoExt.Locations then
+    ulx.GotoExt.Locations = {}
+end
+
+if !ulx.GotoExt.Locations[map] then
+    ulx.GotoExt.Locations[map] = {}
+end
+
+local function save()
+    file.Write("ulx/locs.txt",util.TableToJSON(ulx.GotoExt.Locations,true))
+end
+ulx.GotoExt.Save = save
+
+function ulx.GotoExt.AddLocation(nam, loc)
+    local nam = string.lower(nam)
+    if ulx.GotoExt.Locations[map][nam] then
+        return false, "This location already exists "
+    end
+    ulx.GotoExt.Locations[map][nam] = loc
+    save()
+    return true 
+end
+
+function ulx.GotoExt.RemoveLocation(nam)
+    local nam = string.lower(nam)
+    if !ulx.GotoExt.Locations[map][nam] then
+        return false, "Location does not exist "
+    end
+    ulx.GotoExt.Locations[map][nam] = nil 
+    save()
+    return true
+end
+
+function ulx.GotoExt.GetLocation(nam)
+    local nam = string.lower(nam)
+    if !ulx.GotoExt.Locations[map][nam] then
+        return false, "Location was not found in table"
+    end
+    return ulx.GotoExt.Locations[map][nam]
+end
+
+function ulx.GotoExt.GetTable(nam)
+    return table.Copy(ulx.GotoExt.Locations[map])
+end
+
+
+
 
 // COMMANDS
 
@@ -35,6 +92,27 @@ end
 local freezeall = ulx.command( "Venom", "ulx freezeall", ulx.freezeall, "!freezeall")
 freezeall:defaultAccess( ULib.ACCESS_SUPERADMIN)
 freezeall:help("Freezes all of your entities.")
+
+---------------------------------------------tp commands------------------------------------------
+function ulx.locadd( calling_ply, name)
+    if !name then
+        ulx.fancyLogAdmin("No name was given.")
+    end
+
+    local res,err = ulx.GotoExt.AddLocation(name,calling_ply:GetEyeTraceNoCursor().HitPos)
+    if !res then
+        ulx.fancyLogAdmin(err, "#A")
+        return;
+    end
+    ulx.fancyLogAdmin( calling_ply, "#A has added location #s", name)
+
+end
+
+local locadd = ulx.command( "Location Management", "ulx locadd", ulx.locadd, "!locadd")
+locadd:addParam{ type=ULib.cmds.StringArg, hint="Location name"}
+locadd:defaultAccess( ULib.ACCESS_SUPERADMIN)
+locadd:help("Adds a location that everyone on the server can teleport too.")
+
 
 // HOOKS
 hook.Add("PlayerLeaveVehicle","Ulx_Backseat", function(P,V)
